533f0a241306e1b8e0124a001d18c22167b2820b
Olivier Constant <olivier.constant@thalesgroup.com>
2024-03-27 18:12:20 +0100

    #2 HTML doc generation is needed
    Add missing copyrights
    
    Signed-off-by: Olivier Constant <olivier.constant@thalesgroup.com>

--- plugins/com.thalesgroup.mde.capella.stpa.docgen/generated/com/thalesgroup/mde/capella/stpa/docgen/content/StpaAnalysisElementDocGen.java
+++ plugins/com.thalesgroup.mde.capella.stpa.docgen/generated/com/thalesgroup/mde/capella/stpa/docgen/content/StpaAnalysisElementDocGen.java
@@ -1,4 +1,16 @@
 //Generated with EGF 1.6.3.202110291409
+/*******************************************************************************
+ * Copyright (c) 2020-2024 THALES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    THALES - initial API and implementation
+ *******************************************************************************/
 package com.thalesgroup.mde.capella.stpa.docgen.content;
 
 import org.eclipse.egf.common.helper.*;
