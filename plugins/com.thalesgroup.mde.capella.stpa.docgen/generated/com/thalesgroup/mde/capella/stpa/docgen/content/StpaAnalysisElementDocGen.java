3444a7d9cb9c8982373fa606a13d27f9cbef0fe0
Olivier Constant <olivier.constant@thalesgroup.com>
2024-04-05 19:16:15 +0200

    #2 HTML doc generation is needed
    Add copyrights, update feature and doc
    
    Signed-off-by: Olivier Constant <olivier.constant@thalesgroup.com>

--- plugins/com.thalesgroup.mde.capella.stpa.docgen/generated/com/thalesgroup/mde/capella/stpa/docgen/content/StpaAnalysisElementDocGen.java
+++ plugins/com.thalesgroup.mde.capella.stpa.docgen/generated/com/thalesgroup/mde/capella/stpa/docgen/content/StpaAnalysisElementDocGen.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2020-2024 THALES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    THALES - initial API and implementation
+ *******************************************************************************/
 //Generated with EGF 1.6.3.202110291409
 package com.thalesgroup.mde.capella.stpa.docgen.content;
 
